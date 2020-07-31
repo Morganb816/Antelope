54d08c45eba2f6037710b4627fc9fcc557f30a93
Morgan Brown <morganb816@gmail.com>
2020-07-31 12:55:20 -0400

    fix: exported everything in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const { ClientNetworkValue } = require('./ClientNetworkValue');
 const { ServerNetworkValue } = require('./ServerNetworkValue');
+const { InterpolatedValue } = require('./InterpolatedValue');
+const {runTimesOverTime, moveCloserTo, StackRunner} = require('./util/index');
 
 module.exports = {
     ClientNetworkValue,
-    ServerNetworkValue
+    ServerNetworkValue,
+    InterpolatedValue,
+    StackRunner,
+    runTimesOverTime,
+    moveCloserTo
 }
